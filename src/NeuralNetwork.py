801d03b7f9e0b22718e7b06cbcc61f2b443b27ef
NMGpl <newmastergamingpl+github@gmail.com>
2026-05-17 16:11:52 +0200

    - use adamw

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -7,18 +7,18 @@ import torchvision.transforms as transforms
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.learningRate = 0.1
+        self.learningRate = 0.001
         self.momentum = 0.9
         self.decay = 5e-4
         self.tMax = 125
-        self.learningRateMin = 0.0001
+        self.learningRateMin = 0.0000001
 
         self.InitLayers()
         self.PrepareTransforms()
 
         self.loss_function = nn.CrossEntropyLoss()
-        self.optimizer = optim.SGD(self.parameters(), lr = self.learningRate, momentum = self.momentum, weight_decay = self.decay)
-        # self.optimizer = optim.AdamW(self.parameters(), lr = self.learningRate, weight_decay = self.decay)
+        # self.optimizer = optim.SGD(self.parameters(), lr = self.learningRate, momentum = self.momentum, weight_decay = self.decay)
+        self.optimizer = optim.AdamW(self.parameters(), lr = self.learningRate, weight_decay = self.decay)
         self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max = self.tMax, eta_min = self.learningRateMin)
         
     def InitLayers(self):
